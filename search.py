4c478a90607171b5f83eb9220b111934f5759b0e
CameronWDavis <77825507+CameronWDavis@users.noreply.github.com>
2023-02-21 22:09:41 -0500

    Update search.py

--- search.py
+++ search.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import numpy as np
 import time
 import matplotlib.animation as animation
-import matplotlib as mpl
+import matplotlib.path as mpath
 
-mpl.use('TkAgg') #this is so it works on windows 
+mpl.use('TkAgg')
 from utils import *
 from grid import *
 from algorithms import *
@@ -23,12 +24,33 @@ def gen_polygons(worldfilepath):
             polygons.append(polygon)
     return polygons
 
+
+
+
+def isIllegal(epolygons,point):
+    grid = [[0 for _ in range(50)] for _ in range(50)] #creates a 50 by 50 graph
+    for i in range(len(epolygons)):
+        polygon = [(point.x, point.y) for point in epolygons[i]]
+        path = mpath.Path(polygon)
+        inside = path.contains_points([[point.x,point.y]], radius=-0.1)
+        for j in range(len(polygon)):
+            if inside:
+                return True
+    return  False
+
+
+
+
+
+
+
 if __name__ == "__main__":
     epolygons = gen_polygons('TestingGrid/world1_enclosures.txt')
     tpolygons = gen_polygons('TestingGrid/world1_turfs.txt')
 
     source = Point(8,10)
     dest = Point(43,45)
+
 
     fig, ax = draw_board()
     draw_grids(ax)
@@ -51,13 +73,32 @@ if __name__ == "__main__":
         for i in range(0, len(polygon)):
             draw_green_line(ax, [polygon[i].x, polygon[(i+1)%len(polygon)].x], [polygon[i].y, polygon[(i+1)%len(polygon)].y])
 
-    #### Here call your search to compute and collect res_path
 
-    res_path = [source, Point(25,17), Point(26,17), Point(27,17),  
-                Point(28,17), Point(28,18), Point(28,19), dest]
-    
-    for i in range(len(res_path)-1):
-        draw_result_line(ax, [res_path[i].x, res_path[i+1].x], [res_path[i].y, res_path[i+1].y])
-        plt.pause(0.1)
-    
-    plt.show()
+        #### Here call your search to compute and collect res_path
+        badPoints = []
+        for x in range(50):
+            for y in range(50):
+                p = Point(x,y)
+                bad = isIllegal(epolygons,p)
+                if bad is True:
+                     badPoints.append(p)
+
+        for p in badPoints:
+         print(p)
+
+
+
+
+        res_path = [source, Point(26, 17), Point(27, 17),
+                    Point(28, 17), Point(28, 18), Point(28, 19), dest]
+
+        #res_path = breadthFirstSearch(source,dest,badPoints)
+
+
+        for i in range(len(res_path) - 1):
+            draw_result_line(ax, [res_path[i].x, res_path[i + 1].x], [res_path[i].y, res_path[i + 1].y])
+            plt.pause(0.1)
+
+        plt.show()
+
+
