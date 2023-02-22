df6f261c3e6d39e61bab9ed5337e9f76d5eff5f5
CameronWDavis <77825507+CameronWDavis@users.noreply.github.com>
2023-02-21 22:14:48 -0500

    Update algorithms.py

--- algorithms.py
+++ algorithms.py
@@ -1,22 +1,35 @@
-#def dfsSearch(start,end):
+from utils import *
+def breadthFirstSearch(source,destination,isIllegal):
+    node = source;
+    if node is destination:
+        return node
+    frontier = Queue()
+    frontier.push(node)
+    reached = [node]
+    greaterThan = {node : None}
+    while not frontier.isEmpty():
+        node = frontier.pop()
+        for child in expand(node,isIllegal):
+            if child not in greaterThan.keys():
+                greaterThan[child] = node
+            if (child == destination):
+                return child
+            if child not in reached:
+                reached.append(child)
+                frontier.push(child)
+    return None
 
 
 
 
 
-#def bfsSearch(start,end):
+def expand(point,isIllegal):
+    node = point
+    node = [node.x + 1, node.y + 1]
+    if node in isIllegal:
+        expand(node, isIllegal)
+    else:return node
 
 
 
 
-
-#def gbfsSearch(sld,start,end):
-
-
-
-
-
-
-
-
-#def astarSearch(sld,start,end):
